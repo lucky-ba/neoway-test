94a668204754b15350de895de4e60e5ec34ddfc7
unknown <lucaskatayama12@gmail.com>
2025-10-16 17:38:47 -0300

    feat: change api source to hack news

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,22 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://newsapi.org/v2',
+  baseURL: "https://hacker-news.firebaseio.com/v0/",
   timeout: 10000,
 });
-
-api.interceptors.request.use(
-  (config) => {
-    config.params = {
-        ...config.params,
-        apiKey: "33eb5a6baf50414e8d5f17a3ad4b7ab9",
-    }
-    
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -1,19 +1,41 @@
-import api from './api';
+import api from "./api";
 
 export interface Article {
   title: string;
   description: string;
-  author: string
-  url: string
-  publishDate: string;
+  author: string;
+  url: string;
+  publishedAt: string;
 }
 
-export const getTechNews = async (): Promise<Article[]> => {
+export const getTechNews = async (): Promise<unknown[]> => {
   try {
-    const response = await api.get('/everything', { params: { q: "tech" } });
-    return response.data.articles;
+    const response = await api.get("/topstories.json");
+
+    return await fetchDataInParallel(response.data.slice(0, 100));
   } catch (error) {
-    console.error('Error fetching news:', error);
+    console.error("Error fetching news:", error);
     throw error;
   }
 };
+
+async function fetchDataInParallel(items: number[]) {
+  const promises = items.map(async (item) => {
+    try {
+      const response = await api.get(`/item/${item}.json`);
+      return {
+        title: response.data?.title ?? "No title available",
+        description: response.data?.text ?? "No content available.",
+        url: response.data?.url ?? "#",
+        author: response.data?.by ?? "Data not available.",
+        publishedAt: new Date(
+          response.data?.time ?? 1760646660600
+        ).toLocaleDateString(),
+      };
+    } catch (error) {
+      console.error(`Error fetching data for item:`, error);
+      return { error };
+    }
+  });
+  return await Promise.all(promises);
+}
