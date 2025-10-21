4324d37319eeb349b13e386f05b41f4d1b4fe195
lucky <lucaskatayama12@gmail.com>
2025-10-21 14:05:22 -0300

    fix: add custom elements to vite config to avoid warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/neoway-test/'
-})
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            ["search-input", "virtualized-table", "accessible-modal"].includes(
+              tag
+            ),
+        },
+      },
+    }),
+  ],
+  base: "/neoway-test/",
+});
